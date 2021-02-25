20f281e817f1487f8e65e33d1b0b74f6713a3ef0
EmilioGonzalez08 <78566749+EmilioGonzalez08@users.noreply.github.com>
2021-02-25 12:04:54 -0600

    Update homework.c

--- Homework1/homework.c
+++ Homework1/homework.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 1000
+#define IN 1
+#define OUT 0
 
+int main() {
+    
+  int a, b, c, d;
+  
+  char input[MAX];
+  char output[MAX];
+      
+  printf("Write your HTML code here: ");
+  gets(input);
+  b = strlen(input);
+  c = OUT;
+  printf("\n Plain text: ");
+
+  for (a = 0; a<b ; ++a) {     // im really praying for this to work after making 10000 mistakes 
+    
+    if (input[a] == '<')
+      c = IN;
+    if (input[a] == '>')
+      c = OUT;
+    if (c == OUT)
+      output[a] = input[a];
+    
+    if (output[a] == '>')
+        output[a] = ' ';
+        
+    printf("%c",output[a]); 
+  
+    }
+    
+ }
